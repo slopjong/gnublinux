2eeb05a49cb38b2223eab258436ab7d729afea21
slopjong <git@slopjong.de>
2012-08-03 12:34:59 +0200

    Added another patch for the ADC/PWM driver and a clean script.

---
+++ clean.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd linux*
+
+# remove untracked folders
+rm -rf .tmp_versions drivers/lpc313x_io include/config include/generated/
+
+# remove untracked files
+git clean -f
+
+# revert all the changes done after the last commit
+git stash #save clean_56123j123g124
+git stash drop #clean_56123j123g124
--- patch.sh
+++ patch.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-patches=( '001-mmc-card-detect' '002-gpiolib 003-usb-panic' 'adc-registers' '0006_lpc313x_ADC_PWM' )
-patch_path="gnublin/lpc3131/kernel/2.6.33"
-
 # change to the linux source directory
 cd linux*
+
+## This patch script uses a modified version of the 0006_lpc313x_ADC_PWM patch because
+## the one provided in the gnublin git repository lets the compilation fail.
+## If you want to use the original patch file, uncomment the next line and comment out 
+## the two lines after.
+#patches=( '001-mmc-card-detect' '002-gpiolib 003-usb-panic' 'adc-registers' '0006_lpc313x_ADC_PWM' )
+patch -p1 < ../0006_lpc313x_ADC_PWM_2.patch
+patches=( '001-mmc-card-detect' '002-gpiolib 003-usb-panic' 'adc-registers' )
+
+patch_path="gnublin/lpc3131/kernel/2.6.33"
+
 git checkout 093e9d2f7f730dee86d6734f7a66fc573bcd7027
 
 # patch the source
