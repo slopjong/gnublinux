f6c576c4d003d5bcb93d21ceaa191f3b7560190f
slopjong <git@slopjong.de>
2012-08-17 00:09:52 +0200

    Removed the patch apply in the clean script.

--- clean.sh
+++ clean.sh
@@ -11,3 +11,7 @@ git clean -f
 # revert all the changes done after the last commit
 git stash #save clean_56123j123g124
 git stash drop #clean_56123j123g124
+
+# patch again
+#../patch.sh
+echo "Don't forget to reapply the patch if you rebuild the kernel and its modules."
