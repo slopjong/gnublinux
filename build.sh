4a6b95f39244ab8397a9460eadf8ba3b2de72744
slopjong <git@slopjong.de>
2012-08-17 15:30:10 +0200

    Mounting the SD memory card to sd-card and setting ARCH and CROSS_COMPILE in setenv.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+./setenv.sh
 cd linux*
 
 # compile the kernel and the modules
-make ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabi- kernel
-make ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabi- modules
+make kernel
+make modules
 
 gnublinRoot=""
 
@@ -14,10 +15,19 @@ fi
 
 if [ ! -d "${gnublinRoot}" ]; then
   echo "The path you provided doesn't exist. The built kernel and modules"
-  echo "will be installed to /tmp/gnublin"
-  mkdir -p /tmp/gnublin
-  gnublinRoot="/tmp/gnublin"
+  echo "will be installed to sd-card"
+  mkdir -p ../sd-card
+  gnublinRoot="../sd-card"
+
+  # try to mount the SD memory card
+  # this requires an entry in /etc/fstab such as
+  # UUID=386b3be7-00f3-45e0-832e-1f48c2c3065e /home/slopjong/workspaces/gnublinux/sd-card ext2 users,noauto 0 0
+  # where UUID is the partition UID which can be found out with blkid (run as root)
+  mount ../sd-card > /dev/null 2>&1
 fi
+
+# Start a fakeroot session
+#fakechroot chroot ${gnublinRoot}
 
 backupCounterFile=${gnublinRoot}/backup_counter
 backupCounter=0
@@ -38,7 +48,7 @@ cp ${gnublinRoot}/zImage ${gnublinRoot}/zImage.${backupCounter}
 cp -R ${gnublinRoot}/lib/modules ${gnublinRoot}/lib/modules.${backupCounter}
 
 # install the new kernel and modules
-make ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabi- modules_install INSTALL_MOD_PATH=${gnublinRoot}
+make modules_install INSTALL_MOD_PATH=${gnublinRoot}
 cp arch/arm/boot/zImage ${gnublinRoot}
 
 ## LINKS
--- configure.sh
+++ configure.sh
@@ -1,10 +1,5 @@
-export ARCH=arm 
+#!/bin/bash
 
-# Depending on what cross-compiler toolchain you use
-# you have to uncomment the one you use whil commenting
-# out the other
-export CROSS_COMPILE=arm-none-linux-gnueabi-
-#export CROSS_COMPILE=arm-linux-gnueabi-
-
+./setenv.sh
 cd linux*
 make menuconfig
