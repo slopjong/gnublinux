569d0264e6a48e6e0dd528fc24a6ddfc4ddf5924
slopjong <git@slopjong.de>
2012-08-17 16:04:09 +0200

    Extra script for the mount operation.

--- build.sh
+++ build.sh
@@ -18,12 +18,7 @@ if [ ! -d "${gnublinRoot}" ]; then
   echo "will be installed to sd-card"
   mkdir -p ../sd-card
   gnublinRoot="../sd-card"
-
-  # try to mount the SD memory card
-  # this requires an entry in /etc/fstab such as
-  # UUID=386b3be7-00f3-45e0-832e-1f48c2c3065e /home/slopjong/workspaces/gnublinux/sd-card ext2 users,noauto 0 0
-  # where UUID is the partition UID which can be found out with blkid (run as root)
-  mount ../sd-card > /dev/null 2>&1
+  ./mount.sh
 fi
 
 # Start a fakeroot session
---
+++ mount.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# try to mount the SD memory card
+# this requires an entry in /etc/fstab such as
+# UUID=386b3be7-00f3-45e0-832e-1f48c2c3065e /home/slopjong/workspaces/gnublinux/sd-card ext2 users,noauto 0 0
+# where UUID is the partition UID which can be found out with blkid (run as root)
+mount ../sd-card > /dev/null 2>&1
